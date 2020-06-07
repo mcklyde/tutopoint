6da6c8496135595cdd821e48e314016af15e0091
McKlyde Lagnada <mcklyde@lagnada.com>
2020-06-07 03:29:16 -0700

    Sign in / Sign up

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :tutopoint, Tutopoint.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :tutopoint, TutopointWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
